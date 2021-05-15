d00cfc6d37745b3e3b02e5b708831880fd9d3b42
Crepiks <sayazhan.onlassyn@mail.ru>
2021-05-15 22:13:38 +0600

    feat: implement workers seed file

---
+++ seeds/04_workers.ts
@@ -0,0 +1,33 @@
+import * as Knex from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+  await knex('workers').del();
+
+  await knex('workers').insert([
+    {
+      id: 1,
+      firstName: 'Алия',
+      lastName: 'Галиева',
+      branch: 'Терапевт',
+      description:
+        'Занимается полным терапевтическим лечением. Заведует дневным стационаром.',
+      scheduleId: 1,
+    },
+    {
+      id: 2,
+      firstName: 'Газиз',
+      lastName: 'Курманов',
+      branch: 'Хирург',
+      description: 'Проводит сосудистые операции.',
+      scheduleId: 2,
+    },
+    {
+      id: 3,
+      firstName: 'Жусуп',
+      lastName: 'Тураров',
+      branch: 'Невропатолог',
+      description: 'Консультирует и занимается медикаментозным лечением.',
+      scheduleId: 3,
+    },
+  ]);
+}
