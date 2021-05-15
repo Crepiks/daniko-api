a94e5da84b9c5892ed41e3e153ac14ece93ab4c5
Crepiks <sayazhan.onlassyn@mail.ru>
2021-05-15 14:22:34 +0600

    feat: configure app to serve static files in uploads directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AdminsModule } from './admins/admins.module';
 import { DatabaseModule } from './database/database.module';
 import { WorkersModule } from './workers/workers.module';
 import { ServicesModule } from './services/services.module';
 import { RepositoriesModule } from './data/repositories/repositories.module';
 import { PhotosModule } from './photos/photos.module';
-import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'uploads'),
+    }),
     AdminsModule,
     DatabaseModule,
     WorkersModule,
