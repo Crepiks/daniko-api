3799a6149f25a731948c784f2b8a770d7da8434b
Crepiks <sayazhan.onlassyn@mail.ru>
2021-05-22 18:30:41 +0600

    feat: create single itest image for every test case

--- seeds/02_images.ts
+++ seeds/02_images.ts
@@ -4,11 +4,11 @@ export async function seed(knex: Knex): Promise<void> {
   await knex('images').del();
 
   await knex('images').insert([
-    { id: 1, path: 'worker-image.jpg' },
-    { id: 2, path: 'worker-image.jpg' },
-    { id: 3, path: 'worker-image.jpg' },
-    { id: 4, path: 'service-image.jpg' },
-    { id: 5, path: 'service-image.jpg' },
-    { id: 6, path: 'service-image.jpg' },
+    { id: 1, path: 'worker-image-1.jpg' },
+    { id: 2, path: 'worker-image-2.jpg' },
+    { id: 3, path: 'worker-image-3.jpg' },
+    { id: 4, path: 'service-image-1.jpg' },
+    { id: 5, path: 'service-image-2.jpg' },
+    { id: 6, path: 'service-image-3.jpg' },
   ]);
 }
